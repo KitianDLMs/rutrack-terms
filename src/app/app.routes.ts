0641e886d146253ba713446a16953f0c08c13598
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-02-26 23:41:57 -0300

    ADD: add contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TermsComponent } from './pages/terms/terms.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
   { path: '', component: TermsComponent },
-  { path: 'terminos', component: TermsComponent }
+  { path: 'terminos', component: TermsComponent },
+  { path: 'contacto', component: ContactoComponent }
 ];
