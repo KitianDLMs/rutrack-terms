03a4cfe3a59c7a972daf91c436b6b0367f3f92e6
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-03-06 20:53:58 -0300

    FIX: fix all requestes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { TermsComponent } from './pages/terms/terms.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
-  { path: '', component: TermsComponent },
+  { path: '', component: MainComponent },
   { path: 'terminos', component: TermsComponent },
   { path: 'contacto', component: ContactoComponent }
 ];
